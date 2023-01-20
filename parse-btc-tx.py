7b24eb2193d1a36b002978b92e4775d110d20f95
ayelenrizzalli <ayelenrizzalli@gmail.com>
2023-01-20 10:09:23 -0300

    Adding compact size parsing support

--- parse-btc-tx.py
+++ parse-btc-tx.py
@@ -1,6 +1,7 @@
 # This was the first ever bitcoin transaction where Satoshi Nakamoto sent Hal Finney 10 BTC
 txid_original = 'f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16'
-raw_hex = '0100000001c997a5e56e104102fa209c6a852dd90660a20b2d9c352423edce25857fcd3704000000004847304402204e45e16932b8af514961a1d3a1a25fdf3f4f7732e9d624c6c61548ab5fb8cd410220181522ec8eca07de4860a4acdd12909d831cc56cbbac4622082221a8768d1d0901ffffffff0200ca9a3b00000000434104ae1a62fe09c5f51b13905f07f06b99a2f7159b2225f374cd378d71302fa28414e7aab37397f554a7df5f142c21c1b7303b8a0626f1baded5c72a704f7e6cd84cac00286bee0000000043410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3ac00000000'
+raw_hex_1 = '0100000003efdad76b8da190878c1de4c92fd4aaa0a287984171a4398c1140df11663cb84c010000006b483045022065db71606b84edc291eb2ec55e49ee2fd44afef8b20b4ef88fc2a01c2ba6e963022100dfb24228f2f80574d64a3a2964c5b3d054c14f0bf18c409f72345331271b5020012102a1e806a0c19aaf32363eb19e91a901eafdfc513d13f632f4e2a39f3cb894ad27ffffffff670fa789f11df8b202f380ebc6b4f76fa312f6bfb11494811f00411d7bbb0ae0010000006b4830450221009b5fe2b2bff2a9801725351ae2a8eb410b10b6fecb44edb442ee750e6825f1a4022038e19b3b0e3a95b4a3952dde87efc049d4a72a4424872ab768f7fb3220be4c1e0121032256cb5a8e6d3c9354da72369b939a35febb80d35e6afb50e6f348c20c6c6c05ffffffff52dd5a0965f2d36850f3d2ddeb1457cd72e1cd5a325656af44a3c6ba9f2d42fa010000006c4930460221008a9bf9a1ba9b4125ac9b8cf10423447ad8c7ede3414028237c4c0e0b3b3dc4fd0221009f94721c04b7d4eb33bb1aad61daf98b6ed05dfbf5e3225ae9b3afe24b8924d50121028b04194cb938044761bb93d3917abcce13f910a0500c08e61bdaaf5ea29b5ca0ffffffff02b0c39203000000001976a9148a81571528050b80099821ed0bc4e48ed33e5e4d88ac1f6ab80a010000001976a914963f47c50eaafd07c8b0a8a505c825216a4fee6d88ac00000000'
+raw_hex = "0100000001bbb397fdf39cf8b14a49148861c751543172a6f6500e679e079a7aecfbf7aac400000000fdb50500483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401483045022100e222a0a6816475d85ad28fbeb66e97c931081076dc9655da3afc6c1d81b43f9802204681f9ea9d52a31c9c47cf78b71410ecae6188d7c31495f5f1adfe0df5864a7401ffffffff0180841e00000000001976a9144663e5aab48b092c7478620d867ef2976bce149a88ac00000000"
 
 # Convert from hexa to int
 def convert_raw_amount_to_int(amount):
@@ -12,12 +13,32 @@ def convert_raw_txid(txid):
     amount_decoded_txid = bytes.fromhex(txid)[::-1].hex()
     return(amount_decoded_txid)
 
+def compact_field_size(hex_string):
+    #slice first byte off, first 2 chars, and convert to int
+    n = int(hex_string[:2], 16)
+    sliced_hex_string = hex_string[2::]
+    #if first byte <= 252, return that number as a decimal + new position
+    if (n <= 252): return [n,2]
+    
+    #else, get actual length in next 2**(n-252) bytes
+    else:
+        n = 2 ** (n-252)
+        #slice next n bytes off and convert to int
+        size = sliced_hex_string[:n*2]
+        #compact field size is in little endian, have to flip
+        num = bytes.fromhex(size)[::-1].hex()
+        #return that larger number as a decimal + new position
+        return [int(num , 16), 2+2*n]
+
 # Parsing tx
 version = raw_hex[0:8]
 print('version', version)
-input_count = int(raw_hex[8:10],16)
+input_size_info = compact_field_size(raw_hex[8:])
+print('input_size_info', input_size_info)
+input_count = input_size_info[0]
 print('input_count', input_count)
-position = 10
+position = 8 + input_size_info[1]
+
 inputs = []
 outputs = []
 # Parsing inputs dinamically based on input_count
@@ -26,31 +47,35 @@ for i in range(input_count):
     position += 64
     vout = raw_hex[position:position+8]
     position +=8
-    scriptSig_len = int(raw_hex[position:position+2],16)
-    position +=2
+    scriptSig_info = compact_field_size(raw_hex[position:])
+    print('scriptSig_info', scriptSig_info)
+    scriptSig_len = scriptSig_info[0]
+    position +=scriptSig_info[1]
     scriptSig = raw_hex[position:position+scriptSig_len*2]
     position +=scriptSig_len*2
     sequence = convert_raw_amount_to_int(raw_hex[position:position+8])
     position +=8
-
     inputs.append([txid,vout,scriptSig_len,scriptSig,sequence])
 
 print('inputs', inputs)
 
-output_count = int(raw_hex[position:position+2],16)
-position +=2
+output_size_info = compact_field_size(raw_hex[position:position+2:])
+print('output_size_info', output_size_info)
+output_count = output_size_info[0]
 print('output_count', output_count)
+position = position + output_size_info[1]
 
 # Parsing outputs dinamically based on output_count
 for i in range(output_count):
     amount = convert_raw_amount_to_int(raw_hex[position:position+16]) #sats
     position += 16
-    scriptPubKey_len = int(raw_hex[position:position+2],16)
-    position +=2
+    scriptPubKey_info = compact_field_size(raw_hex[position:])
+    print('scriptPubKey_info', scriptPubKey_info)
+    scriptPubKey_len = scriptPubKey_info[0]
+    position +=scriptPubKey_info[1]
     scriptPubKey = raw_hex[position: position+scriptPubKey_len*2]
     position +=scriptPubKey_len*2
     outputs.append([amount,scriptPubKey_len,scriptPubKey])
-
 
 
 print('outputs', outputs)
